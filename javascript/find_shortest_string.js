d47b42dbec3ac230b98294a7bdccd9119cdee3e2
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-06-18 16:13:08 -0600

    ez compared to yesterday

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,11 @@
 function findShortestString(arr) {
-  // type your code here
+  let webLength = []
+  for (const string of arr){
+    webLength.push(string.length)
+  }
+  const smolString = Math.min(...webLength)
+  const indx = webLength.indexOf(smolString)
+  return arr[indx]
 }
 
 if (require.main === module) {
@@ -24,3 +30,7 @@ module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// function takes in array of strings
+// calculates the .length of each string and puts that in an array
+// then gets the shortest number and returns the index of that number, then returning the same index of the original array
